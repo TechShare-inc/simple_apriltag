30d46bb71c5b6da075a06cc4293c4d10d0288bc7
Wataru Oshima <wataru.bb.tokyo@gmail.com>
2024-05-09 11:59:51 +0900

    fix convert 3D pose in robot coordinate system
    ロボット座標に変換した３次元TFの配信

--- src/simple_tag.cpp
+++ src/simple_tag.cpp
@@ -33,10 +33,10 @@ Pose2D TagCalculate::convertTo2DPose(const apriltag_pose_t& pose) {
 
 Pose3D TagCalculate::convertTo3DPose(const apriltag_pose_t& pose) {
     Pose3D pose3D;
-    pose3D.x = matd_get(pose.t, 0, 0);
-    pose3D.y = matd_get(pose.t, 1, 0);
-    pose3D.z = matd_get(pose.t, 2, 0);
-    pose3D.roll = - atan2(matd_get(pose.R, 1, 0), matd_get(pose.R, 0, 0)); // tagのz軸周り
+    pose3D.x = matd_get(pose.t, 2, 0); // tagのz方向
+    pose3D.y = - matd_get(pose.t, 0, 0); // tagのx方向
+    pose3D.z = - matd_get(pose.t, 1, 0); // tagのy方向
+    pose3D.roll = atan2(matd_get(pose.R, 1, 0), matd_get(pose.R, 0, 0)); // tagのz軸周り
     pose3D.pitch = - atan2(matd_get(pose.R, 2, 1), matd_get(pose.R, 2, 2)); // tagのx軸周り
     pose3D.yaw = - atan2(-matd_get(pose.R, 2, 0), sqrt(pow(matd_get(pose.R, 2, 1), 2) + pow(matd_get(pose.R, 2, 2), 2))); // tagのy軸周り
     return pose3D;
